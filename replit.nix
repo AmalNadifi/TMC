6219c04eb530f3e7627a8296c5196b4dc20578f0
nadiaMahfoud <129057048+nadiaMahfoud@users.noreply.github.com>
2024-04-25 16:22:50 +0000

    Tmp
    mxamc5hn0z59492701628~

---
+++ replit.nix
@@ -0,0 +1,17 @@
+{pkgs}: {
+  deps = [
+    pkgs.glibcLocales
+    pkgs.libxcrypt
+    pkgs.pgadmin4
+    pkgs.iana-etc
+    pkgs.bash
+    pkgs.cacert
+    pkgs.rustc
+    pkgs.openssl
+    pkgs.libiconv
+    pkgs.cargo
+    pkgs.libmysqlclient
+    pkgs.pkg-config
+    pkgs.arrow-cpp
+  ];
+}
